00d166391c5d5b40e5feeed5f96f3d017c83f5e6
Tyler <claypooltyler00@gmail.com>
2020-04-29 22:54:19 -0500

    Finished repo for specality interface

---
+++ HealthPairService/HealthPairDomain/Interfaces/ISpecialtyRepository.cs
@@ -0,0 +1,28 @@
+﻿using HealthPairDomain.InnerModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPairDomain.Interfaces
+{
+    public interface ISpecialtyRepository
+    {
+        Task<IEnumerable<InnerSpeciality>> GetspecialityAsync(string search = null);
+
+        Task<InnerSpeciality> GetspecialityByIdAsync(int id);
+
+        //Task<IEnumerable<InnerSpeciality>> GetspecialityByPatientNameAsync(string search = null);
+
+        //Task<IEnumerable<InnerSpeciality>> GetspecialityByProviderId(int id);
+
+        Task<bool> SpecialityExistAsync(int id);
+
+        Task<InnerSpeciality> AddspecialityAsync(InnerSpeciality speciality);
+
+        Task<bool> RemoveSpecialityAsync(int id);
+
+        EntityState Changed(InnerSpeciality speciality);
+    }
+}
