e1bd160fa979ec06223218c686fc6e3cd6b1b5b9
Paul Edwards <pauldedwards95@gmail.com>
2020-05-03 13:51:17 -0500

    added needed data in startup for authorization

--- HealthPairService/HealthPairAPI/Startup.cs
+++ HealthPairService/HealthPairAPI/Startup.cs
@@ -8,9 +8,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 using HealthPairDataAccess.DataModels;
 using HealthPairDataAccess.Repositories;
 using HealthPairDomain.Interfaces;
+using HealthPairAPI.Helpers;
 
 [assembly: ApiController]
 namespace HealthPairAPI
@@ -87,6 +93,46 @@ namespace HealthPairAPI
 
                 options.ReturnHttpNotAcceptable = true;
                 options.SuppressAsyncSuffixInActionNames = false;
+            });
+
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context =>
+                    {
+                        var userService = context.HttpContext.RequestServices.GetRequiredService<IPatientRepository>();
+                        var userId = int.Parse(context.Principal.Identity.Name);
+                        var user = userService.GetPatientByIdAsync(userId);
+                        if (user == null)
+                        {
+                            // return unauthorized if user no longer exists
+                            context.Fail("Unauthorized");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
             });
         }
 
